40eadd6f9bd34ff9aa6eb2c21952d82403ef269d
Yasin Hajilou <yasinhaji78@yahoo.com>
2025-07-12 15:27:05 -0400

    Added 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,66 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [good, setGood] = useState(0);
+  let [bad , setBad] = useState(0);
+  let [neutral , setNeutral] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Title content="give feedback" />
 
-export default App
+      <div>
+        <AppButton
+          onClick={() => {
+            setGood(good + 1)
+          }}
+          title="good"
+        />
+        <AppButton
+          onClick={() => {
+            setNeutral(neutral + 1)
+          }}
+          title="neutral"
+        />
+        <AppButton
+          onClick={() => {
+            setBad(bad + 1)
+          }}
+          title="bad"
+        />
+      </div>
+
+      <Title content="Statistics" />
+
+      <div>
+        <ListElement content="good" count={good} />
+        <ListElement content="neutral" count={neutral} />
+        <ListElement content="bad" count={bad} />
+      </div>
+    </>
+  );
+};
+
+const Title = ({ content }) => {
+  return (
+    <>
+      <h1>{content}</h1>
+    </>
+  );
+};
+
+const ListElement = ({ content, count }) => {
+  return (
+    <>
+      <h4>
+        {content} {count}
+      </h4>
+    </>
+  );
+};
+
+const AppButton = ({ onClick, title }) => {
+  return <button onClick={onClick}>{title}</button>;
+};
+
+export default App;
